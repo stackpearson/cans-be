7d7d7db523ff6313dfe7234ad129017bc6dffc49
Sawyer <sawyer.pearson92@gmail.com>
2021-08-03 21:59:51 -0600

    delete can completed

--- cans/cans-model.js
+++ cans/cans-model.js
@@ -27,41 +27,23 @@ function findUserCans(id) {
 async function addCans(can) {
     try {
         const [id] = await db('cans').insert(can, 'id');
-        console.log('addCan called')
         return findCanById(id);
     } catch (error) {
         throw error;
     }
 }
 
-// async function addServiceRelation(serviceRelation) {
-//    try {
-//     // const service =  await db('user_vehicles').where('user_vehicles.vehicle_id', '=', `${serviceRelation.vehicle_id}`).update({service_id: `${serviceRelation.service_id}`})
-//     const [id] = await db('user_vehicles').insert(serviceRelation, 'id')
-//     return findRelationById(id)
-//     // console.log(service)
-//    } catch (error) {
-//        throw error;
-//    } 
-// }
+async function removeCan(id) {
+    try {
+        return db('cans')
+            .where({ id })
+            .del();
+    } catch (error) {
+       throw error; 
+    }
 
-// async function removeService(id) {
-//     try {
-//         await removeServiceRelation(id)
-//         return db('user_services')
-//             .where({ id })
-//             .del();
-//     } catch (error) {
-//        throw error; 
-//     }
+}
 
-// }
-
-// function removeServiceRelation(id) {
-//     return db('user_vehicles')
-//         .where('service_id', '=', `${id}`)
-//         .del();
-// }
 
 
 
@@ -72,5 +54,6 @@ module.exports = {
     findCanById,
     addCans,
     allCans,
-    findUserCans
+    findUserCans,
+    removeCan
 }
--- cans/cans-router.js
+++ cans/cans-router.js
@@ -41,7 +41,7 @@ router.get('/user-cans/:id', restricted, (req, res) => {
 })
 
 
-router.post('/new-can/:id', restricted, async (req, res) => {
+router.post('/new-can/:id', restricted, (req, res) => {
     let user_id = req.body.user_id
     let can_name = req.body.can_name;
     let can_text = req.body.can_text;
@@ -53,72 +53,19 @@ router.post('/new-can/:id', restricted, async (req, res) => {
         'can_text': can_text
     }
 
-    console.log("can_details: ", can_details)
-
     Cans.addCans(can_details)
         .then(newCan => {
             res.status(201).json(newCan)
         })
         .catch(err => res.send(err))
-
-    // try {
-    //     if (savedCan) {
-    //         res.status(201).json({canAdded: savedCAn})
-    //     } else {
-    //         res.status(500).json({message: 'server error from cans-router, can not added'})
-    //     }
-    // } catch (err) {
-    //     next({apiCode:500, apiMessage:'server error', ...err})
-    // }
 })
 
-// router.post('/:id', restricted, async (req, res) => {
-//     const { user_id } = req.params;
-//     let can_name = req.body.can_name;
-//     let can_text = req.body.can_text;
- 
 
-//     let can_details = {
-//         'can_name': can_name,
-//         'can_text': can_text,
-//         'user_id': user_id,
-//         'next_service_date': nextServiceDate,
-//         'next_service_mileage': nextServiceMileage,
-//         'service_notes': serviceNotes
-//     }
-//     let vehicle_id = req.body.vehicle_id;
-//     let user_id = req.body.user_id;
-
-//     const savedService = await Cans.addServices(serviceDetails)
-    
-//     const serviceRelation = {
-//         user_id: user_id,
-//         vehicle_id: vehicle_id,
-//         service_id: savedService.id
-//     }
-
-//     const savedRelation = await Cans.addServiceRelation(serviceRelation)
-    
-//     try {
-//         if (savedRelation) {
-//             res.status(201).json({serviceAdded: savedService, savedRelation: savedRelation })
-//         } else {
-//             res.status(500).json({message: 'server error from services router, vehicle not added'})
-//             console.log(error)
-//         }
-        
-        
-        
-//     } catch (err) {
-//         next({apiCode:500, apiMessage:'error adding service from service router', ...err})
-//     }
-// })
-
-router.delete('/:id', (req, res) => {
+router.delete('/delete-can/:id', (req, res) => {
     let id = req.params.id;
-    Cans.removeService(id)
+    Cans.removeCan(id)
         .then((del) => {
-            res.status(200).json({message: 'service deleted'})
+            res.status(200).json({message: 'can deleted'})
         })
         .catch((err) => {
             console.log(err)
